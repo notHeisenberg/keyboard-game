6d86ea5c1b60317534ebed6ecd6a1fb05c0a59f4
notHeisenberg007 <mahfuzantor2@gmail.com>
2024-02-24 01:56:54 +0600

    all-done

--- js/events.js
+++ js/events.js
@@ -1,18 +1,71 @@
-function  continueGame(){
+document.addEventListener('keyup', afterPressWork)
+
+function afterPressWork(event) {
+
+    console.log(event.key);
+
+    const keyPressed = event.key;
+
+    const expectedKey = document.getElementById('current-text').innerText.toLocaleLowerCase();
+    console.log(expectedKey)
+
+    if (keyPressed === expectedKey) {
+
+        console.log('milse')
+        continueGame()
+
+        removeBackgroundColor(expectedKey);
+
+        let scoreValue = getElementValueById('score-btn');
+        console.log(scoreValue)
+        
+        scoreValue += 1;
+        console.log(scoreValue)
+        setElementValueById('score-btn',scoreValue);
+
+
+    }
+    else{
+        console.log('mile nai')
+
+        let lifeValue = getElementValueById('life-btn');
+        console.log(lifeValue)
+        lifeValue-=1;
+        setElementValueById('life-btn',lifeValue);
+        if (lifeValue <= 0 ){
+            removeBackgroundColor(expectedKey);
+            score()
+        }
+
+    }
+}
+
+
+function continueGame() {
     const alphaBate = generateAlphaBate();
     console.log('random value', alphaBate);
 
-    setAlphaBate('current-text',alphaBate)
-    // console.log('initial value',document.getElementById('current-text').innerText);
-
+    setAlphaBate('current-text', alphaBate);
     // const elementText = document.getElementById('current-text');
     // elementText.innerText = alphaBate;
+
+    setBackgroundColor(alphaBate);
 
 }
 
 
 function play() {
     hideElement('home-section');
-    showElement('play-section')
+    hideElement('score-section');
+    setElementValueById('life-btn',5)
+    setElementValueById('score-btn',0)
+    showElement('play-section');
     continueGame();
+}
+
+function score(){
+    hideElement('play-section');
+    showElement('score-section');
+    const lastScore =getElementValueById('score-btn');
+    setElementValueById('final-score',lastScore);
 }
--- js/utility.js
+++ js/utility.js
@@ -1,7 +1,27 @@
+function setBackgroundColor(elementID){
+    const elementText = document.getElementById(elementID);
+    elementText.classList.add('bg-[#FFA500]')
+}
+function removeBackgroundColor(elementID){
+    const elementText = document.getElementById(elementID);
+    elementText.classList.remove('bg-[#FFA500]')
+}
+
 
 function setAlphaBate(elementID,value){
     const elementText = document.getElementById(elementID);
     elementText.innerText = value;
+}
+
+function getElementValueById(elementID){
+    const getKey = document.getElementById(elementID)
+    const getValue = getKey.innerText;
+    return parseInt(getValue);
+}
+
+function setElementValueById(elementID,value){
+    const setKey = document.getElementById(elementID);
+    setKey.innerText = value;
 }
 
 function generateAlphaBate(){
